89eba97923c9cd00c6fbef75f02f29e74b57962c
zhangyk <zhangyk@tju.edu.cn>
2019-04-18 16:34:23 +0800

    Add project files.

---
+++ WpfAppCollectItems/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppCollectItems
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAddPerson_Click(object sender, RoutedEventArgs e)
+        {
+            //Add(new PersonName("Willa", "Cather"));
+            if (this.Resources["NameListData"] is NameList xx)
+            {
+                xx.Add(new PersonName($"Willa{xx.Count.ToString("000")}", $"Cather{xx.Count.ToString("000")}"));
+            }
+        }
+    }
+}
---
+++ WpfAppCollectItems/NameList.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfAppCollectItems
+{
+    public class NameConverter : IMultiValueConverter
+
+    {
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+
+        {
+
+            string name;
+            switch ((string)parameter)
+            {
+
+                case "FormatLastFirst":
+
+                    name = values[1] + ", " + values[0];
+
+                    break;
+                case "FormatNormal":
+
+                    name = values[0] + " " + values[1];
+
+                    break;
+                default:
+
+                    name = values[0] + " " + values[1] + " Email:" + values[2];
+
+                    break;
+
+            }
+
+
+
+            return name;
+
+        }
+
+
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+
+        {
+
+            var splitValues = ((string)value).Split(' ');
+
+            return splitValues;
+
+        }
+
+    }
+    public class NameList : ObservableCollection<PersonName>
+    {
+        public NameList() : base()
+        {
+            Add(new PersonName("Willa", "Cather"));
+            Add(new PersonName("Isak", "Dinesen"));
+            Add(new PersonName("Victor", "Hugo"));
+            Add(new PersonName("Jules", "Verne"));
+            Add(new PersonName("Wang", "Lina"));
+            Add(new PersonName("Mei", "Daoheng"));
+        }
+    }
+
+    public class PersonName
+    {
+        private string firstName;
+        private string lastName;
+        private static int count=0;
+        public PersonName(string first, string last,string email="foo1{0}@tju.edu.cn")
+        {
+            this.firstName = first;
+            this.lastName = last;
+            count++;
+            this.EmailAddress = string.Format(email,count);
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string EmailAddress { get;set; }
+    }
+}
